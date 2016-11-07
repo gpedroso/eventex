a8c5542b742dfce5c0769eb0dd3f948da1dba91c
Gabriel Pedroso <gpedroso@gmail.com>
2016-11-07 21:45:35 -0200

    Valida cpf

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -2,10 +2,33 @@ from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscriptionFormTest(TestCase):
-	def setUp(self):
-		self.form = SubscriptionForm()
-
 	def test_form_has_fields(self):
 		""" Form must have 4 fields """
+		form = SubscriptionForm()
 		expected = ['name', 'cpf', 'email', 'phone']
-		self.assertSequenceEqual(expected, list(self.form.fields))
+		self.assertSequenceEqual(expected, list(form.fields))
+
+	def test_cpf_is_digit(self):
+		""" CPF must only accept digits. """
+		form = self.make_validated_forms(cpf='ABCD5678901')
+		self.assertFormErrorCode(form, 'cpf', 'digits')
+
+	def test_cpf_has_11_digits(self):
+		""" CPF must have 11 digits """
+		form = self.make_validated_forms(cpf='1234')
+		self.assertFormErrorCode(form, 'cpf', 'length')
+
+	def assertFormErrorCode(self, form, field, code):
+		errors = form.errors.as_data()
+		errors_list = errors[field]
+		exception = errors_list[0]
+		self.assertEqual(code, exception.code)
+
+
+	def make_validated_forms(self, **kwargs):
+		valid = dict(name='Gabriel Pedroso', cpf='12345678901', 
+					email='gpedroso@gmail.com', phone='12-996582594')
+		data = dict(valid, **kwargs)
+		form = SubscriptionForm(data)
+		form.is_valid()
+		return form		
