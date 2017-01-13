7a37652e6a05c3f3d2d58e5ecfd1914eee6d5e17
Gabriel Pedroso <gpedroso@gmail.com>
2017-01-13 17:14:45 -0200

    simplifica com modelform

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,25 +1,41 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
 
-def validate_cpf(value): 
-	if not value.isdigit():
-		raise ValidationError('CPF deve conter apenas números','digits') 
-	if len(value) != 11:
-		raise ValidationError('CPF deve conter 11 dígitos','length')
 
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email', required=False)
     phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
-    	name = self.cleaned_data['name']
-    	words = [w.capitalize() for w in name.split()]
-    	return ' '.join(words)
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
 
     def clean(self):
-    	if (self.cleaned_data.get('email') == '' and self.cleaned_data.get('phone') == ''):
-    		raise ValidationError('Informe seu email ou telefone.')
+        if (self.cleaned_data.get('email') == '' and self.cleaned_data.get('phone') == ''):
+            raise ValidationError('Informe seu email ou telefone.')
 
-    	return self.cleaned_data
+        return self.cleaned_data
+
+class SubscriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
+
+    def clean(self):
+        ''' linha usada para que use o clean da classe model forms, sem essa chamada o metodo clean é sobrescrito
+        sem a validação de unicidade, por exemplo.'''
+        self.cleaned_data = super().clean()
+
+        if (self.cleaned_data.get('email') == '' and self.cleaned_data.get('phone') == ''):
+            raise ValidationError('Informe seu email ou telefone.')
+
+        return self.cleaned_data
--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,11 +1,12 @@
 import uuid
 from django.db import models
+from eventex.subscriptions.validators import validate_cpf
 
 class Subscription(models.Model):
 	name = models.CharField('Nome', max_length=100)
-	cpf = models.CharField('CPF', max_length=11)
-	email = models.EmailField('E-mail')
-	phone = models.CharField('Telefone', max_length=20)
+	cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+	email = models.EmailField('E-mail', blank=True)
+	phone = models.CharField('Telefone', max_length=20, blank=True)
 	created_at = models.DateTimeField('Criado em', auto_now_add=True)
 	hashId = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
 	paid = models.BooleanField('Pago', default=False)
